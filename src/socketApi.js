4cfd0d82c2c10e1b9b1f27509f9a8dcd77d09d8d
onurcanolum <onurcanolum@gmail.com>
2020-03-29 22:59:32 +0300

    newUser

--- src/socketApi.js
+++ src/socketApi.js
@@ -3,8 +3,23 @@ const io = socketio();
 
 const socketApi =  {};
 socketApi.io = io;
+
+const users = [ ];
+
 io.on('connection', (socket) => {
-    console.log('a user connected.')
+    console.log('a user connected.');
+    socket.on('newUser', (data) => {
+        const defaultData = {
+            id: socket.id,
+            position: {
+                x: 0,
+                y: 0
+            }
+        };
+        const userData = Object.assign(data, defaultData);
+        users.push(userData);
+        console.log(users)
+    });
 });
 
 module.exports = socketApi;
